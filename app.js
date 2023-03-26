8334288adce2e8f773d012de62cd5e992c3a432a
Quinten vD <89836942+QuintenvDriel@users.noreply.github.com>
2023-03-26 21:52:23 +0200

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import { DecisionTree } from "./libraries/decisiontree.js"
 import { VegaTree } from "./libraries/vegatree.js"
 
 const csvFile = "./data/diabetes.csv"
-const trainingLabel = "Gender"
+const trainingLabel = "pregnancies"
 const ignoredColumns = ['Name']
 
 // inladen csv data
@@ -11,9 +11,31 @@ function loadData() {
         download: true,
         header: true,
         dynamicTyping: true,
-        complete: (results) => console.log(results.data) // train het model met deze data
+        complete: (results) => checkData(results.data) // train het model met deze data
     })
 }
+
+
+function checkData(data) {
+    console.table(data)
+}
+let data = loadData = "data"
+shuffleArray(data)
+
+function shuffleArray(arr) {
+    const arr = []
+    arr.sort((function(){return 0.5 - Math.random()}))
+}
+let arr = shuffleArray = "arr"
+trainData(arr)
+testData(arr)
+
+// model trainen met net verkregen data
+
+let trainData = data.slice(0, Math.floor(data.length * 0.8))
+let testData = data.slice(Math.floor(data.length * 0.8) + 1)
+
+trainModel
 
 //
 // MACHINE LEARNING - Bouw de Decision Tree
@@ -21,7 +43,8 @@ function loadData() {
 function trainModel(data) {
 
     let ignoredColumns = ["Outcome"]
-    let label = "label"
+    let label = "Outcome"
+    // let label = "label"
     
     let decisionTree = new DecisionTree({
         ignoredAttributes: ignoredColumns,
@@ -30,6 +53,7 @@ function trainModel(data) {
     })
 
     // Teken de boomstructuur - DOM element, breedte, hoogte, decision tree
+    let json = decisionTree.toJSON()
     let visual = new VegaTree('#view', 800, 400, decisionTree.toJSON())
 }
 
